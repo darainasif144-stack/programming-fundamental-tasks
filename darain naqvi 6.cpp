235a8a508b1e9943f3d39b237dae6f67f90166b1
darainasif144-stack <darainasif144@gmail.com>
2025-11-23 20:12:40 +0500

    Add files via upload

---
+++ darain naqvi 6.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout <<"==========================================" <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"       STUDENT RESULT CARD                " <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"==========================================" <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"Name:         Ali Raza                    " <<endl;
+    cout <<"Roll NO:      1023                        " <<endl;
+    cout <<"Department:   Computer Science            " <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"------------------------------------------" <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"Subjects       Marks                      " <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"------------------------------------------" <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"English        85                         " <<endl;
+    cout <<"Math           90                         " <<endl;
+    cout <<"Physics        80                         " <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"------------------------------------------" <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"Total marks:   255/300                    " <<endl;
+    cout <<"Percentage:    85%                        " <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"==========================================" <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"Grade:          A+                        " <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"==========================================" <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"Congratulations Ali Raza!                 " <<endl;
+    cout <<"                                          " <<endl;
+    cout <<"==========================================" <<endl;
+    
+    
