e939bb20cb0528375194cb703735d5960185c56d
darainasif144-stack <darainasif144@gmail.com>
2025-11-23 19:20:27 +0500

    Add files via upload

---
+++ darain naqvi 3.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout <<"\n--------------------------------------------------------";
+	cout <<"\n\tCertificate of achievement";
+	cout <<"\n--------------------------------------------------------";
+	cout <<"\nThis is to certify that 'Ali Raza'\n has successfully completed the course\n' with A+ Grade";
+	cout <<"\n--------------------------------------------------------";
+	return 0;
+}
