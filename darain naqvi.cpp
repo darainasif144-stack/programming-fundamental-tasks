1231b8f3797266fef0c6ba80066353d8d2f440e6
darainasif144-stack <darainasif144@gmail.com>
2025-11-23 19:19:04 +0500

    Add files via upload

---
+++ darain naqvi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout <<"-------------------------------------------" << endl;
+	cout <<"                                           " << endl;
+	cout <<"           Student ID Card                 " << endl;
+	cout <<"                                           " << endl;
+	cout <<"-------------------------------------------" << endl;
+	cout <<"                                           " << endl;
+	cout <<"Name:      darain naqvi                  " << endl;
+	cout <<"                                           " << endl;
+	cout <<"Roll No:    2025-SET-41                    " << endl;
+	cout <<"                                           " << endl;
+	cout <<"Department: Software Engineering           " << endl;
+	cout <<"                                           " << endl;
+	cout <<"Session:    2025-2029                      " << endl;
+	cout <<"                                           " << endl;
+	cout <<"-------------------------------------------" << endl;
+	return 0;
+}
